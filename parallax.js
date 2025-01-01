8befbf091f6000ce35c4ec2d50857e1c72d4482b
losrobbos <robert.ristock@googlemail.com>
2025-01-01 21:08:31 +0100

    Replaced top left shift by transform for fast compositor only change

--- parallax.js
+++ parallax.js
@@ -7,14 +7,19 @@ const heroTitle = document.querySelector("h1")
 window.onscroll = (ev) => {
     const currY = window.scrollY
 
+    // TODO: apply relative TRANSFORM to current position instead of layout props top/left
+
     // move moon down
-    imgMoon.style.top = currY + "px";
+    // imgMoon.style.top = currY + "px";
+    imgMoon.style.translate = "0 " + currY*0.8 + "px"
 
     // move title from center to left (negative x)
-    heroTitle.style.left = (550 - currY) + "px"
+    // heroTitle.style.left = (550 - currY) + "px"
+    heroTitle.style.translate = `calc(-50% - ${currY*0.5}px) calc(-50% + ${currY*0.5}px)`
 
     // move back mountains slowly down
-    imgMountainBack.style.top = (currY*0.3) + "px"
+    // imgMountainBack.style.top = (currY*0.3) + "px"
+    imgMountainBack.style.translate = "0 " + currY*0.4 + "px"
 
     // move 
 }
